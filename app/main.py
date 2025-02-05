9eec8e8efe4225651e27fe88ddd815ec6938fa16
“n84374161” <nayer.nasef.hanallah1@h-partners.com>
2025-02-05 16:58:02 +0200

    ..

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from .database import Base, engine
 from .config import DATABASE_URL, ALGORITHM, SECRET_KEY
 
 # Create all database tables
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to SnippetCode API"}
+
+
+@app.post('/')
+def create_user():
+    
